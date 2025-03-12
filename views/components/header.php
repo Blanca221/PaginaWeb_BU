3e7b58ac0b2ea70105acab4f9089c9e9c7b2ef3a
Blanca221 <blancamiranda@ceroca.cat>
2025-03-12 01:59:02 +0100

    icono de registro y iniciar sesion funcionando

--- views/components/header.php
+++ views/components/header.php
@@ -40,4 +40,8 @@
             </div>
         </div>
     </div>
+
+    <!-- Scripts necesarios para el dropdown -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </header> 
